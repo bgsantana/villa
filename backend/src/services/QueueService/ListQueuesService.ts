82862d48e6760b52ea1db4f958b53817f58b31d4
bgsantana <gabrielfla96@gmail.com>
2023-09-30 00:52:10 -0300

    first commit

---
+++ backend/src/services/QueueService/ListQueuesService.ts
@@ -0,0 +1,9 @@
+import Queue from "../../models/Queue";
+
+const ListQueuesService = async (): Promise<Queue[]> => {
+  const queues = await Queue.findAll({ order: [["id", "ASC"]] });
+
+  return queues;
+};
+
+export default ListQueuesService;
